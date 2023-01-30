d3caec2f7e3583e249dba071fb295e641b65cc89
climlock <chumakov.mikhail18@gmail.com>
2023-01-31 00:14:06 +0200

    Test Commit

--- src/Dispatcher.java
+++ src/Dispatcher.java
@@ -1,5 +1,98 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Dispatcher {
     static public void main (String[] args){
-        System.out.println("hello World!");
+        interface1();
+
+
     }
+    static public void interface1(){
+        List<User> Users = new ArrayList<>();
+        System.out.println("---------------------------------------------");
+        System.out.println("            Enter your action:               ");
+        System.out.println("             1 - Login                       ");
+        System.out.println("             2 - Register                    ");
+        System.out.println("             3 - Exi                         ");
+        System.out.println("---------------------------------------------");
+        System.out.println("Your choose:                                 ");
+
+        switch ((new Scanner(System.in)).nextInt()){
+            case 1: {
+                User admin = interfaceLogin(Users);
+                if (admin != null){
+                    System.out.println("Success!");
+                    interface2(admin);
+                } else{
+                    System.out.println("Try again");
+                    interface1();
+                }
+                break;
+            }
+            case 2: {
+                Users.add(interfaceRegister());
+                for (User temp : Users) {
+                    System.out.println("L: " + temp.getLogin());
+                    System.out.println("P: " + temp.getPassword());
+                }
+                interface1();
+                break;
+            }
+            case 3: {
+                System.out.println("Good luck!"); break;
+            }
+            default: {
+                System.out.println("Incorrect answer! >>>>>");
+                interface1();
+
+            }
+        }
+    }
+    public static User interfaceLogin(List<User> Users){
+        System.out.println("---------------------------------------------");
+        System.out.println("Login: ");
+        String login = (new Scanner(System.in).nextLine());
+        System.out.println("Password: ");
+        String password = (new Scanner(System.in).nextLine());
+        System.out.println(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
+        for (User temp: Users) {
+            System.out.println(temp.getLogin());
+            if (temp.getLogin().equals(login) && temp.getPassword().equals(password)) {
+                System.out.println("ZAEBIS");
+                return temp;
+            }
+        }
+        return null;
+    }
+    public static User interfaceRegister(){
+        String[] data = new String[2];
+        System.out.println("---------------------------------------------");
+        System.out.println("Set Login: ");
+        data[0] = (new Scanner(System.in).nextLine());
+        System.out.println("Set Password: ");
+        data[1] = (new Scanner(System.in).nextLine());
+        System.out.println("---------------------------------------------");
+        return (new User(data[0], data[1]));
+    }
+    public static void interface2(User User){
+        System.out.println("---------------------------------------------");
+        System.out.println("Hello " + User);
+        System.out.println("---------------------------------------------");
+    }
+
+}
+class User {
+    private String login;
+    private String password;
+    User(String login, String password){
+        this.login = login; this.password = password;
+    }
+    public String getLogin(){
+        return this.login;
+    }
+    public String getPassword(){
+        return this.password;
+    }
+
 }
